574a84634ad7a135f293007b5db8817acf648a5b
collinsjie <37666095+collinsjie@users.noreply.github.com>
2023-03-20 03:21:07 +0100

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,33 @@
+# # # try:
+# # #     age = int(input('Enter your age.'))
+# # # except ValueError:
+# # #     print('you entered an alphabet, but you can enter a number')
+# # # else:
+# # #     #print('you are [age] year old')
+    
+# # #finally:
+# #     #print('You are done')
+# # #while True:
+# #     #age = input('')
+# #     #if type(age)== int:
+# #         #print('you are [age] years old')
+# #     #else:q
+# #         #print('wrong input')
+        
+# # age = input()
+# # try:
+# #     age=int(age)
+# # except valueerror:
+# #     print('wrong input type')
+# # else:
+# #     print(age)
+# #for i in range(1,10,2):
+#     #print(i)
+for num in range(1,13):
+    for mul in range(1,13):
+        print(f'{num} X {mul} = {num*mul}' )
+# Age = 3
+# print(f'You are {Age} years old')
+# print('You are {}of age'.format(Age))
+# print('You are %s at %d' %('fool', Age))
+
